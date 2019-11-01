4553950bfba300312a5976db92db6148b517b7c5
sophieMdl <sophie.michaudel@gmail.com>
2019-11-01 17:34:18 +0100

    init project

---
+++ front/src/components/app.js
@@ -0,0 +1,12 @@
+import { h, Component } from 'preact'
+import Header from './header'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div id="app">
+        <Header />
+      </div>
+    )
+  }
+}
---
+++ front/src/components/header/index.js
@@ -0,0 +1,10 @@
+import { h } from 'preact'
+import style from './style.css'
+
+const Header = () => (
+  <header class={style.header}>
+    <h1>Travel My Way</h1>
+  </header>
+)
+
+export default Header
