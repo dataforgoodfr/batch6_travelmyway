dde44a9e9db289fc9f4dead45b622e26bfb96925
Adrien Quillet <adrienquillet@gmail.com>
2019-12-05 20:26:56 +0100

    [detailedResult Page] 🔎 searchbar recap + minor style

--- front/src/detailedResult/DetailedResultContainer.jsx
+++ front/src/detailedResult/DetailedResultContainer.jsx
@@ -5,34 +5,36 @@ import DetailedResultCard from './DetailedResultCard'
 import ResultCard from '../resultList/ResultCard'
 import EcologyCard from './EcologyCard'
 import StepsCard from './StepsCard'
-import SearchRecap from '../search/SearchRecap'
+import SearchBarRecap from './SearchBarRecap'
 import { Grid } from 'semantic-ui-react'
 import { getCO2InKg } from '../journey.utils'
 
-const DetailedResult = ({ result, results }) => {
-  const totalCO2InKg = getCO2InKg(result.total_gCO2)
+const DetailedResultContainer = ({ selectedJourney, results }) => {
+  const totalCO2InKg = getCO2InKg(selectedJourney.total_gCO2)
+  console.log('result', selectedJourney)
+  console.log('results', results)
   return (
-    <div className="page-results">
+    <div className="page-detailed-results">
       <Header />
-      <SearchRecap></SearchRecap>
+      <SearchBarRecap />
       <main className="content-wrapper">
         <Grid>
           <Grid.Row>
             <Grid.Column>
-              <DetailedResultCard result={result} />
+              <DetailedResultCard selectedJourney={selectedJourney} />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width={8}>
-              <StepsCard steps={result.journey_steps} />
+              <StepsCard steps={selectedJourney.journey_steps} />
             </Grid.Column>
             <Grid.Column width={8}>
               <h3 className="ecology">{totalCO2InKg} de CO2 émis</h3>
               <EcologyCard />
               <p>Les autres trajets écologiques</p>
-              {results.map(result => (
-                <Link key={result.id} to={`/results/${result.id}`}>
-                  <ResultCard result={result} />
+              {results.map(item => (
+                <Link key={item.id} to={`/results/${item.id}`}>
+                  <ResultCard result={item} />
                 </Link>
               ))}
               <Link to="/results">Voir toutes les options ></Link>
@@ -44,4 +46,4 @@ const DetailedResult = ({ result, results }) => {
   )
 }
 
-export default DetailedResult
+export default DetailedResultContainer
